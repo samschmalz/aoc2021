6810b9cd8cf87784cfd47032765b3caef15097ea
Sam Schmalzried <me@samschmalzried.com>
2021-12-06 10:41:39 -0600

    puzzle 1 complete

--- day1/puzzle1.pl
+++ day1/puzzle1.pl
@@ -1,16 +1,18 @@
 use strict;
+use warnings;
 
 my $inc_count = 0;
 my $prev_line = -1;
 
-open(my $input, "<", "input.txt");
+open(my $input, "<", "input.txt") or die "Can't open input.txt";
 
 while (my $line = <$input>) {
 	if ($line > $prev_line and $prev_line != -1) {
 		$inc_count++;
 	}
+	$prev_line = $line;
 }
 
 close($input);
 
-print($inc_count);
+print($inc_count."\n");
