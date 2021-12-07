7a00b035529d087fb79899579cfc41db6dc42a87
Sam Schmalzried <me@samschmalzried.com>
2021-12-06 20:34:57 -0600

    attempted puzzle 2 for day 1

---
+++ day1/puzzle2.pl
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+my $inc_count = 0;
+
+open(my $input, "<", "input.txt") or die "Can't open input.txt";
+
+my @lines = <$input>;
+
+close($input);
+
+sub sum_three {
+	my $index = @_;
+	return $lines[$index] + $lines[$index+1] + $lines[$index+2];
+}
+
+my @sums = ();
+
+for (my $i = 0; $i < $#lines; $i++) {
+	my $temp_sum = sum_three($i);
+	push(@sums, $temp_sum);
+	if ($i != 0 and $sums[$i] > $sums[$i-1]) {
+		$inc_count++;
+	}
+}
+
+print($inc_count."\n");
