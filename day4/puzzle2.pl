dcd2e62417554070158d87a4c6258cacb1e272c4
Sam Schmalzried <me@samschmalzried.com>
2021-12-16 12:44:35 -0600

    Finished day 4, puzzle 2

---
+++ day4/puzzle2.pl
@@ -0,0 +1,76 @@
+use strict;
+use warnings;
+
+use Data::Dumper;
+use List::Util qw( sum );
+
+use File::Basename;
+use lib dirname (__FILE__);
+
+use Bingo;
+
+sub ltrim { my $s = shift; $s =~ s/^\s+//;       return $s };
+
+open(my $input, "<", "input.txt");
+
+my @rolls = split(/,/,<$input>);
+my @lines = <$input>;
+
+close($input);
+
+splice(@lines, 0, 1); # remove empty line at the very beginning
+
+my @boards = ();
+
+for (my $i = 0; $i < @lines; $i += 6) {
+	my @board = ();
+	for (my $j = 0; $j < 5; $j++) {
+		my @board_row = split(/ +/, ltrim($lines[$i+$j]), 5);
+		push(@board, [ @board_row ]);
+	}
+	push(@boards, [ @board ]);
+}
+
+my @board_scores = ();
+my @winners = ();
+
+foreach (@boards) {
+	my @score = ();
+	for (my $i = 0; $i < 5; $i++) {
+		$score[$i] = [0,0,0,0,0];
+	}
+	push(@board_scores, [ @score ]);
+	push(@winners, 0);
+}
+
+foreach (@rolls) {
+	my $roll = $_;
+	my $winner = -1;
+	foreach my $i (0 .. $#boards) {
+		$board_scores[$i] = Bingo::check_number($roll, $boards[$i], $board_scores[$i]);
+		if (Bingo::check_win($board_scores[$i])) {
+			$winners[$i] = 1;
+			if (sum(@winners) == @winners) {
+				$winner = $i;
+				last;
+			}
+		}
+	}
+	if ($winner > -1) {
+		my @victor = @{$boards[$winner]};
+		my @victor_score = @{$board_scores[$winner]};
+		my $victor_sum = 0;
+		foreach my $i ( 0 .. $#victor ) {
+			my @row = @{ $victor[$i] };
+			foreach my $j ( 0 .. $#row ) {
+				if ( $victor_score[$i][$j] == 0 ) {
+					$victor_sum += $victor[$i][$j];
+				}
+			}
+		}
+		$victor_sum *= $roll;
+		print("Final winner: board $winner!\n");
+		print("Final Score: $victor_sum\n");
+		last;
+	}
+}
